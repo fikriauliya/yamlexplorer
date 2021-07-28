067c475ea5ed5938d04ece9a3a7b413030280a07
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2021-07-28 07:22:55 +0700

    (feat) Use MapSlice for ordering, remove order meta

--- parser/parser.go
+++ parser/parser.go
@@ -8,63 +8,71 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-func extractHeader(rows []interface{}, order []string) []string {
-	headers := make(map[string]bool)
-	headerRow := rows[0].(map[interface{}]interface{})
+func extractHeader(mapSlice yaml.MapSlice) ([]string, error) {
+	var dataSlice []interface{}
+	for _, v := range mapSlice {
+		if v.Key == "data" {
+			dataSlice = v.Value.([]interface{})
+			break
+		}
+	}
+	if len(dataSlice) == 0 {
+		return nil, fmt.Errorf("no data found")
+	}
 
-	for header := range headerRow {
-		headers[header.(string)] = true
+	headerSlice := dataSlice[0].(yaml.MapSlice)
+	res := make([]string, len(headerSlice))
+	for i, val := range headerSlice {
+		res[i] = val.Key.(string)
 	}
-	result := make([]string, 0)
-	if order != nil {
-		for _, o := range order {
-			if _, ok := headers[o]; ok {
-				result = append(result, o)
-			}
-		}
-	} else {
-		for k := range headers {
-			result = append(result, k)
-		}
-	}
-	return result
+	return res, nil
 }
 
-func extractBody(header []string, rows []interface{}) []([]string) {
-	body := make([]([]string), len(rows))
+func extractBody(header []string, mapSlice yaml.MapSlice) ([]([]string), error) {
+	var dataSlice []interface{}
+	for _, v := range mapSlice {
+		if v.Key == "data" {
+			dataSlice = v.Value.([]interface{})
+			break
+		}
+	}
+	if len(dataSlice) == 0 {
+		return nil, fmt.Errorf("no data found")
+	}
+	body := make([]([]string), len(dataSlice))
 	headerOrder := make(map[string]int)
 	for i, h := range header {
 		headerOrder[h] = i
 	}
-	for i, row := range rows {
-		row := row.(map[interface{}]interface{})
+	for i, row := range dataSlice {
+		row := row.(yaml.MapSlice)
 		body[i] = make([]string, len(row))
-		for k, value := range row {
+		for _, v := range row {
 			var newValue string
-			switch value.(type) {
+			switch v.Value.(type) {
 			case string:
-				newValue = value.(string)
+				newValue = v.Value.(string)
 			case int:
-				newValue = fmt.Sprint(value.(int))
+				newValue = fmt.Sprint(v.Value.(int))
 			}
-			j := headerOrder[k.(string)]
+			j := headerOrder[v.Key.(string)]
 			body[i][j] = newValue
 		}
 	}
-	return body
+	return body, nil
 }
 
-func readAndUnmarshallYAML(path string) (*map[interface{}]interface{}, error) {
+func readAndUnmarshallYAML(path string) (yaml.MapSlice, error) {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read the file: %s", err)
 	}
-	m := make(map[interface{}]interface{})
+	var m yaml.MapSlice
 	err = yaml.Unmarshal([]byte(data), &m)
 	if err != nil {
 		return nil, fmt.Errorf("failed to unmarshall YAML: %s", err)
 	}
-	return &m, err
+	return m, err
 }
 
 func ParseYAML(path string) (*entity.Table, error) {
@@ -73,30 +81,15 @@ func ParseYAML(path string) (*entity.Table, error) {
 		return nil, err
 	}
 
-	var order []string
-	meta, ok := (*m)[interface{}("_meta")]
-	if ok {
-		meta := meta.(map[interface{}]interface{})
-		o, ok := meta[interface{}("order")]
-		if ok {
-			o := o.([]interface{})
-			order = make([]string, len(o))
-			for i, item := range o {
-				order[i] = item.(string)
-			}
-		}
+	header, err := extractHeader(m)
+	if err != nil {
+		return nil, err
 	}
-
-	for k, v := range *m {
-		if k != "_meta" {
-			rows := v.([]interface{})
-
-			header := extractHeader(rows, order)
-			body := extractBody(header, rows)
-			return &entity.Table{
-				Header: header,
-				Body:   body}, nil
-		}
+	body, err := extractBody(header, m)
+	if err != nil {
+		return nil, err
 	}
-	return nil, fmt.Errorf("invalid YAML")
+	return &entity.Table{
+		Header: header,
+		Body:   body}, nil
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -3,73 +3,104 @@ package parser
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"gopkg.in/yaml.v2"
 )
 
 var _ = Describe("Parser", func() {
 	Describe("Extract Header", func() {
 		It("returns only the header", func() {
-			sut := make([]interface{}, 2)
-			sut[0] = map[interface{}]interface{}{
-				"name": "alice",
-				"age":  "23"}
-			sut[1] = map[interface{}]interface{}{
-				"name": "bob",
-				"age":  "30",
+			sut := yaml.MapSlice{
+				yaml.MapItem{
+					Key: "data",
+					Value: []interface{}{
+						yaml.MapSlice{
+							yaml.MapItem{Key: "name", Value: "alice"},
+							yaml.MapItem{Key: "age", Value: "23"},
+						},
+						yaml.MapSlice{
+							yaml.MapItem{Key: "name", Value: "bob"},
+							yaml.MapItem{Key: "age", Value: "30"},
+						},
+					},
+				},
 			}
 
-			result := extractHeader([]interface{}(sut), nil)
+			result, err := extractHeader(sut)
 
+			Expect(err).ToNot(HaveOccurred())
 			Expect(result).To(ConsistOf("name", "age"))
 		})
-		Context("when order is specified", func() {
-			It("always returns with the same sequence as the given order", func() {
-				sut := make([]interface{}, 2)
-				sut[0] = map[interface{}]interface{}{
-					"name": "alice",
-					"age":  "23"}
-				sut[1] = map[interface{}]interface{}{
-					"name": "bob",
-					"age":  "30",
-				}
-				order := []string{"age", "name"}
+		It("always returns with the same sequence as the input", func() {
+			sut := yaml.MapSlice{
+				yaml.MapItem{
+					Key: "data",
+					Value: []interface{}{
+						yaml.MapSlice{
+							yaml.MapItem{Key: "age", Value: "23"},
+							yaml.MapItem{Key: "name", Value: "alice"},
+						},
+						yaml.MapSlice{
+							yaml.MapItem{Key: "age", Value: "30"},
+							yaml.MapItem{Key: "name", Value: "bob"},
+						},
+					},
+				},
+			}
 
-				result := extractHeader([]interface{}(sut), order)
+			result, err := extractHeader(sut)
 
-				Expect(result).To(BeEquivalentTo([]string{"age", "name"}))
-			})
+			Expect(err).ToNot(HaveOccurred())
+			Expect(result).To(BeEquivalentTo([]string{"age", "name"}))
 		})
 	})
 	Describe("Extract Body", func() {
 		It("returns only the body", func() {
-			sut := make([]interface{}, 2)
-			sut[0] = map[interface{}]interface{}{
-				"name": "alice",
-				"age":  "23"}
-			sut[1] = map[interface{}]interface{}{
-				"name": "bob",
-				"age":  "30",
+			sut := yaml.MapSlice{
+				yaml.MapItem{
+					Key: "data",
+					Value: []interface{}{
+						yaml.MapSlice{
+							yaml.MapItem{Key: "name", Value: "alice"},
+							yaml.MapItem{Key: "age", Value: "23"},
+						},
+						yaml.MapSlice{
+							yaml.MapItem{Key: "name", Value: "bob"},
+							yaml.MapItem{Key: "age", Value: "30"},
+						},
+					},
+				},
 			}
-			result := extractBody([]string{"name", "age"}, sut)
+			result, err := extractBody([]string{"name", "age"}, sut)
 
+			Expect(err).ToNot(HaveOccurred())
 			Expect(result[0]).To(ConsistOf("alice", "23"))
 			Expect(result[1]).To(ConsistOf("bob", "30"))
 		})
 		It("returns the body with the same order as the header", func() {
-			sut := make([]interface{}, 2)
-			sut[0] = map[interface{}]interface{}{
-				"name": "alice",
-				"age":  "23"}
-			sut[1] = map[interface{}]interface{}{
-				"name": "bob",
-				"age":  "30",
+			sut := yaml.MapSlice{
+				yaml.MapItem{
+					Key: "data",
+					Value: []interface{}{
+						yaml.MapSlice{
+							yaml.MapItem{Key: "name", Value: "alice"},
+							yaml.MapItem{Key: "age", Value: "23"},
+						},
+						yaml.MapSlice{
+							yaml.MapItem{Key: "name", Value: "bob"},
+							yaml.MapItem{Key: "age", Value: "30"},
+						},
+					},
+				},
 			}
-			result := extractBody([]string{"name", "age"}, sut)
+			result, err := extractBody([]string{"name", "age"}, sut)
 
+			Expect(err).ToNot(HaveOccurred())
 			Expect(result[0]).To(BeEquivalentTo([]string{"alice", "23"}))
 			Expect(result[1]).To(BeEquivalentTo([]string{"bob", "30"}))
 
-			result = extractBody([]string{"age", "name"}, sut)
+			result, err = extractBody([]string{"age", "name"}, sut)
 
+			Expect(err).ToNot(HaveOccurred())
 			Expect(result[0]).To(BeEquivalentTo([]string{"23", "alice"}))
 			Expect(result[1]).To(BeEquivalentTo([]string{"30", "bob"}))
 		})
